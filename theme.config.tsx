1a2dae2e26f800039a71ec290d1b5927b651f21d
so-c <so_c321@hotmail.co.jp>
2023-03-19 14:24:23 +0900

    #32 不要ファイルを削除, ヘッダ、フッタを修正

--- theme.config.tsx
+++ theme.config.tsx
@@ -1,8 +1,22 @@
+import Image from 'next/image'
+
 const themeConfig = {
-    logo: <span>My Nextra Documentation</span>,
-    project: {
-        link: 'https://github.com/shuding/nextra',
+  logo: <span>Mirror House Entrance</span>,
+  project: {
+    link: "https://twitter.com/SO_C",
+    icon: <Image src="/images/soshi-hood.png" width={36} height={36} alt="twitter" />,
+  },
+  feedback: {
+    content: () => {
+      return null;
     },
-}
+  },
+  editLink: {
+    component: <></>,
+  },
+  footer: {
+    text: <span>{new Date().getFullYear()} © Soshi Kagami</span>
+  }
+};
 
 export default themeConfig;
