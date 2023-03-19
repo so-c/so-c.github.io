f5f1ee476fb6f0b539d31cec5e156b2b5d5fbda1
so-c <so_c321@hotmail.co.jp>
2023-03-19 16:54:39 +0900

    #32 SSGできるように修正。不要ファイル削除

--- theme.config.tsx
+++ theme.config.tsx
@@ -1,10 +1,17 @@
-import Image from 'next/image'
+import Image from "next/image";
 
 const themeConfig = {
   logo: <span>Mirror House Entrance</span>,
   project: {
     link: "https://twitter.com/SO_C",
-    icon: <Image src="/images/soshi-hood.png" width={36} height={36} alt="twitter" />,
+    icon: (
+      <Image
+        src="/images/soshi-hood.png"
+        width={36}
+        height={36}
+        alt="twitter"
+      />
+    ),
   },
   feedback: {
     content: () => {
@@ -12,11 +19,13 @@ const themeConfig = {
     },
   },
   editLink: {
-    component: <></>,
+    component: () => {
+      return <></>;
+    },
   },
   footer: {
-    text: <span>{new Date().getFullYear()} © Soshi Kagami</span>
-  }
+    text: <span>{new Date().getFullYear()} © Soshi Kagami</span>,
+  },
 };
 
 export default themeConfig;
