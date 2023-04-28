5c8475d2adf24fb7a5d8eacbe1e366e7df7ed5ad
so-c <so_c321@hotmail.co.jp>
2023-04-28 09:02:48 +0900

    close #36 twitter cardを変更

---
+++ next-seo.config.ts
@@ -0,0 +1,15 @@
+const nextSeoConfig = {
+  openGraph: {
+    type: 'website',
+    locale: 'ja_JP',
+    url: 'https://so-c.github.io/',
+    siteName: 'Mirror House Entrance',
+  },
+  twitter: {
+    handle: '@SO_C',
+    site: '@SO_C',
+    cardType: 'summary',
+  },
+};
+
+export default nextSeoConfig;
